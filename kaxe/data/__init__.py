3a901f71f9d9ad07459eac97d330c619b19ae143
Valter Daugberg <59122895+valteryde@users.noreply.github.com>
2023-10-27 13:45:14 +0200

    Delete kaxe directory

--- kaxe/data/__init__.py
+++
@@ -1,2 +0,0 @@
-
-from .excel import loadExcel
--- kaxe/plot/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .point import Points
-from .plot import Plot, Marker
-from .helper import intersectLines
-# from .function import *
-# from .equation import Equation
-# from .point import Points
