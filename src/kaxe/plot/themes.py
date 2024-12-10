417f4786e2b825b5a2d0c89131050e37fdcc8f1d
Valter Yde Daugberg <valteryde@hotmail.com>
2024-12-10 10:26:03 +0100

    More themes

--- src/kaxe/plot/themes.py
+++ src/kaxe/plot/themes.py
@@ -1,5 +1,8 @@
 
 class Themes:
 
-    A4Full = {"width":3000, "height":2000, "fontSize":40}
-    A4Half = {"width":1500, "height":1500, "fontSize":40}
+    A4Medium = {"axis.stepSizeBand": [400, 250], "marker.tickLength":50, "marker.tickWidth":4,  "height":3000, "width":4000, "fontSize":128}
+    A4Large = {"axis.stepSizeBand": [400, 250], "marker.tickLength":50, "marker.tickWidth":4,  "height":3000, "width":5000, "fontSize":128}
+    A4Small = {"axis.stepSizeBand": [500, 350], "marker.tickLength":50, "marker.tickWidth":4,  "height":2000, "width":3000, "fontSize":128}
+    A4Slim = {"axis.stepSizeBand": [400, 250], "marker.tickLength":50, "marker.tickWidth":4, "height":1300, "width":4000, "fontSize":128}
+    A4Mini = {"axis.stepSizeBand":[500, 300], "marker.tickLength":50, "marker.tickWidth":4, "height":2000, "width":2500, "fontSize":128}
